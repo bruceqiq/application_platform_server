5ca33261f1f95e4e3cda4052c9cd9062a48442db
peter <peter@qq.com>
2021-01-20 19:32:12 +0800

    更新

--- app/Model/Common/AppToken.php
+++ app/Model/Common/AppToken.php
@@ -25,6 +25,18 @@ class AppToken extends BaseModel
         'token',
         'expire_time',
         'remark',
+        'status',
+        'cache_time'
+    ];
+
+    protected $hidden = [
+        'deleted_at',
+    ];
+
+    protected $appends = [
+        'status_text',
+        'expire_text',
+        'expire_status',
     ];
 
     public $searchFields = [
@@ -40,6 +52,8 @@ class AppToken extends BaseModel
         'cloud_platform_id',
         'created_at',
         'updated_at',
+        'status',
+        'cache_time',
     ];
 
     public function platform()
